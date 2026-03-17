c706d8300943ad6991694a0200cbd081b5ef93ee
silvertakana <titmitna@gmail.com>
2026-03-17 18:11:54 +1300

    chore: add tsconfig.json to plugin packages, update package.json for npm publish

---
+++ packages/wwv-plugin-camera/dist/CameraDetail.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import type { GeoEntity } from "@worldwideview/wwv-plugin-sdk";
+interface CameraDetailProps {
+    entity: GeoEntity;
+}
+export declare const CameraDetail: React.FC<CameraDetailProps>;
+export {};
+//# sourceMappingURL=CameraDetail.d.ts.map
---
+++ packages/wwv-plugin-camera/dist/CameraSettings.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+export declare const CameraSettings: React.FC<{
+    pluginId: string;
+}>;
+//# sourceMappingURL=CameraSettings.d.ts.map
